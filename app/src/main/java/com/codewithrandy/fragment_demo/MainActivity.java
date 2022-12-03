a50707ef43e8dd112ae6614a85f5f22e04b22da9
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2022-12-03 10:26:26 -0600

    replace fragment

--- app/src/main/java/com/codewithrandy/fragment_demo/MainActivity.java
+++ app/src/main/java/com/codewithrandy/fragment_demo/MainActivity.java
@@ -1,14 +1,45 @@
 package com.codewithrandy.fragment_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnFrag1, btnFrag2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnFrag1 = findViewById(R.id.btnFrag1);
+        btnFrag2 = findViewById(R.id.btnFrag2);
+
+        btnFrag1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new Frag1());
+            }
+        });
+
+        btnFrag2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new Frag2());
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
