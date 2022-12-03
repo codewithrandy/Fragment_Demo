ada56fe79d1333a92846775ef13d5057d523aff5
Randy McKown <72272050+nerdvoodoo@users.noreply.github.com>
2022-12-03 10:18:26 -0600

    setup fragment layouts

---
+++ app/src/main/java/com/codewithrandy/fragment_demo/Frag1.java
@@ -0,0 +1,22 @@
+package com.codewithrandy.fragment_demo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Frag1 extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_frag1, container, false);
+        return view;
+    }
+}
